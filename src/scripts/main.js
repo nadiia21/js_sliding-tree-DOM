03ea5242dca06844c3b4835aa9223bed2424689e
Nadiia <zoryvchaknv@gmail.com>
2024-12-04 20:01:43 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const lis = tree.querySelectorAll('li');
+
+lis.forEach((el) => {
+  const text = el.childNodes[0].nodeValue.trim();
+  const span = document.createElement('span');
+
+  span.textContent = text;
+  el.childNodes[0].replaceWith(span);
+});
+
+tree.addEventListener('click', (ev) => {
+  ev.stopPropagation();
+
+  const span = ev.target.closest('span');
+
+  if (!span) {
+    return;
+  }
+
+  const ul = span.nextElementSibling;
+
+  if (ul) {
+    if (ul.style.display === 'block' || ul.style.display === '') {
+      ul.style.display = 'none';
+    } else {
+      ul.style.display = 'block';
+    }
+  }
+});
